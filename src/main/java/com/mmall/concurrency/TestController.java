00ba4d1a01dabbdc2299c7e826a50e04c77d79b0
ningfei.li <ningfei.li@gloryholiday.com>
2018-12-02 18:17:35 +0800

    lombok

---
+++ src/main/java/com/mmall/concurrency/TestController.java
@@ -0,0 +1,23 @@
+/*
+ * TestController.java  * Created on  2018/12/2
+ * Copyright (c) 2018 YueTu
+ * YueTu TECHNOLOGY CO.,LTD. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * YueTu Ltd. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you
+ * entered into with YueTu Ltd.
+ */
+package com.mmall.concurrency;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ling.zhang
+ */
+@Controller
+@Slf4j
+public class TestController {
+}

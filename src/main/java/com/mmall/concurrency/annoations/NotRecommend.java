f1415c192787afb40dec97c6a96bc23a265e8b58
ningfei.li <ningfei.li@gloryholiday.com>
2018-12-02 17:54:22 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/NotRecommend.java
@@ -0,0 +1,27 @@
+/*
+ * ThreadSafe.java  * Created on  2018/12/2
+ * Copyright (c) 2018 YueTu
+ * YueTu TECHNOLOGY CO.,LTD. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * YueTu Ltd. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you
+ * entered into with YueTu Ltd.
+ */
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author ling.zhang
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+}

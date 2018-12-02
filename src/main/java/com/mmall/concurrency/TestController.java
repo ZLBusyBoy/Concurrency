02732e5ea0fc535353e76395393da6c4d77d7c8f
ningfei.li <ningfei.li@gloryholiday.com>
2018-12-02 21:18:17 +0800

    test

--- src/main/java/com/mmall/concurrency/TestController.java
+++ src/main/java/com/mmall/concurrency/TestController.java
@@ -13,6 +13,8 @@ package com.mmall.concurrency;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author ling.zhang
@@ -20,4 +22,10 @@ import org.springframework.stereotype.Controller;
 @Controller
 @Slf4j
 public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "test";
+    }
 }

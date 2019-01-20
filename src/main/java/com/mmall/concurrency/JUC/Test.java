756e7eb7eb1950cc11f30e8e7520e56de77b9c54
ningfei.li <ningfei.li@gloryholiday.com>
2019-01-20 20:22:30 +0800

    countDownLatch show test

---
+++ src/main/java/com/mmall/concurrency/JUC/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Test.java  * Created on  2019/1/5
+ * Copyright (c) 2019 YueTu
+ * YueTu TECHNOLOGY CO.,LTD. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * YueTu Ltd. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you
+ * entered into with YueTu Ltd.
+ */
+package com.mmall.concurrency.JUC;
+
+/**
+ * @author ling.zhang
+ */
+public class Test {
+}
---
+++ src/main/java/com/mmall/concurrency/JUC/tools/CountDownLatchShow.java
@@ -0,0 +1,24 @@
+/*
+ * CountDownLatchShow.java  * Created on  2019/1/5
+ * Copyright (c) 2019 YueTu
+ * YueTu TECHNOLOGY CO.,LTD. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * YueTu Ltd. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you
+ * entered into with YueTu Ltd.
+ */
+package com.mmall.concurrency.JUC.tools;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author ling.zhang
+ */
+public class CountDownLatchShow {
+
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+    }
+}
